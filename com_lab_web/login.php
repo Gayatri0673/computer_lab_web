b3cf6f5c7db5f1bf8464fa9ece6f42b0a7a32570
rohini <sonkamblerohini8@gmail.com>
2025-09-29 20:49:01 +0530

    changes in login page

---
+++ com_lab_web/login.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous" />
+</head>
+
+<body>
+
+    <div class="container d-flex flex-column align-items-center justify-content-center" style="height: 100vh;">
+
+        <h3 class="text-center mb-3">Create User</h3>
+
+        <div class="parent border-3 rounded shadow p-5">
+            <!-- admin input -->
+            <label for="text">Admin :-</label>
+            <input type="text" id="Admin" placeholder="Admin" class="rounded p-2 ms-lg-4" /><br>
+
+            <!-- branch input -->
+            <label for="text">Branch :-</label>
+            <input type="text" id="Branch" placeholder="Enter Branch" class="mt-3 rounded p-2 ms-lg-4" /><br>
+
+            <!-- post inpute -->
+            <label for="text">Post :-</label>
+            <input type="text" id="Post" placeholder="Enter Post" class="mt-3 rounded p-2 ms-lg-5" /><br>
+
+            <!-- lab inpute -->
+            <label for="text">Lab :-</label>
+            <input type="text" id="Lab" placeholder="Enter Lab" class="mt-3 rounded p-2 ms-lg-5" /><br>
+
+            <!-- password input -->
+            <label for="text">Password :-</label>
+            <input type="password" id="Password" placeholder="Enter Password" class="mt-3 rounded p-2 ms-lg-2" /><br>
+
+            <!-- user name inpute -->
+
+            <label for="text">User Name :-</label>
+            <input type="text" id="Uname" placeholder="Enter User Name " class="mt-3 rounded p-2" /><br>
+
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
+            crossorigin="anonymous"></script>
+</body>
+
+</html>
