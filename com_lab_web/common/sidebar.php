531467868c58e35f48a113dc8ae99a46ff328d30
Gayatri0673 <wajurkargayatri@gmail.com>
2025-10-02 15:07:11 +0530

    adding new files (#1)

---
+++ com_lab_web/common/sidebar.php
@@ -0,0 +1,25 @@
+<aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
+    <div class="sidebar app-brand demo">
+        <a href="index.php" class="app-brand-link">
+            <span class="app-brand-text demo menu-text fw-bolder ms-2"></span>
+            <img src="assets/img/logo/logo-csmss.png" class="w-100" alt="csmss-logo">
+
+        </a>
+        <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto d-block d-xl-none">
+            <i class="bx bx-chevron-left bx-sm align-middle"></i>
+        </a>
+    
+
+        <a href="#" class="active"><i class="bi bi-speedometer2 me-2"></i>Dashboard</a>
+        <a href="#"><i class="bi bi-house me-2"></i>Computer Department</a>
+        <a href="#"><i class="bi bi-building me-2"></i>AI-ML</a>
+        <a href="#"><i class="bi bi-file-earmark-text me-2"></i>Audit Report FY</a>
+        <a href="#"><i class="bi bi-briefcase me-2"></i>Placement</a>
+        <a href="#"><i class="bi bi-megaphone me-2"></i>Marquee</a>
+        <a href="#"><i class="bi bi-telephone me-2"></i>Contact</a>
+        <a href="#"><i class="bi bi-bell me-2"></i>Notice</a>
+        <a href="#"><i class="bi bi-journal-text me-2"></i>Curriculum</a>
+        <a href="#"><i class="bi bi-newspaper me-2"></i>News & Events</a>
+        <a href="#"><i class="bi bi-people me-2"></i>Department</a>
+    </div>
+</aside>
