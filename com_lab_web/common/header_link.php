0eb9071e327a5ce34bc44fe1d14c5b8cb194e09e
Gayatri0673 <wajurkargayatri@gmail.com>
2025-10-03 19:56:52 +0530

    changes in old files (#5)

--- com_lab_web/common/header_link.php
+++ com_lab_web/common/header_link.php
@@ -56,6 +56,7 @@
       href="../assets/vendor/libs/apex-charts/apex-charts.css"
     />
 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Page CSS -->
 
     <!-- Helpers -->
