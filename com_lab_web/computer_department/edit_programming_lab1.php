6273fe4daab9f3c5e082f6d6e92d4e021dc9d2d1
rohini919 <sonkamblerohini8@gmail.com>
2025-12-05 13:28:06 +0530

    crud in files (#11)

--- com_lab_web/computer_department/edit_programming_lab1.php
+++ com_lab_web/computer_department/edit_programming_lab1.php
@@ -1,157 +1,189 @@
 <?php
+$conn = mysqli_connect("localhost", "root", "", "computer");
 
+$accession_no = trim($_GET['accession_no']);
+$project_query = "SELECT * FROM programming_lab1 WHERE accession_no ='$accession_no'";
+$project_query_run = mysqli_query($conn, $project_query);
 
-if (isset($_GET['id'])) {
-    $id = $_GET['id'];
-    $query = "SELECT * FROM toppers WHERE id='$id'";
-    $result = mysqli_query($conn, $query);
+if (isset($_POST['edit_btn'])) {
+    // Correct POST variables
+    $update_acc_no = $_POST['update_acc_no'];
+    $Name_of_Equipment = $_POST['Name_of_Equipment'];
+    $accession_no = $_POST['accession_no'];
+    $Configration = $_POST['Configration'];
+    $Maintenance = $_POST['Maintenance'];
+    $System_no = $_POST['System_no'];
+    $GRN_no = $_POST['GRN_no'];
+    $Unit_Rate = $_POST['Unit_Rate'];
+    $Quantity = $_POST['Quantity'];
+    $Amount = $_POST['Amount'];
+    $Remark = $_POST['Remark'];
 
-    if (mysqli_num_rows($result) > 0) {
-        $row = mysqli_fetch_assoc($result);
+    // Correct UPDATE SQL query
+    $query_update ="UPDATE programming_lab1 SET 
+            name_of_equipment = '$Name_of_Equipment',
+            accession_no = '$accession_no',
+            configration = '$Configration',
+            Maintenance = '$Maintenance',
+            System_no = '$System_no',
+            GRN_no = '$GRN_no',
+            Unit_Rate = '$Unit_Rate',
+            Quantity = '$Quantity',
+            Amount = '$Amount',
+            Remark = '$Remark'
+        WHERE accession_no = '$update_acc_no'";
+
+    $query_update_run = mysqli_query($conn, $query_update);
+
+    if ($query_update_run) {
+        echo 'Data updated';
+        header("Location: programming_lab1.php");
     } else {
-        echo "No record found!";
-        exit();
+        echo 'Data NOT updated: ' . mysqli_error($conn);
     }
 }
 
-if (isset($_POST['update'])) {
-    $id = $_POST['id'];
-    $name = $_POST['student_name'];
-    $percentage = $_POST['percentage'];
-    $class_awarded = $_POST['class_awarded'];
-    $department = $_POST['department'];
-    $year = $_POST['year'];
 
-    $update_query = "UPDATE toppers 
-                     SET student_name='$name', percentage='$percentage', class_awarded='$class_awarded', 
-                         department='$department', year='$year' 
-                     WHERE id='$id'";
 
-    if (mysqli_query($conn, $update_query)) {
-        header("Location: show_Toppers.php"); // redirect back
-        exit();
-    } else {
-        echo "Error updating record: " . mysqli_error($conn);
-    }
-}
+if (mysqli_num_rows($project_query_run)) {
+
+    while ($row = mysqli_fetch_array($project_query_run)) {
+
 ?>
-<!DOCTYPE html>
-<html
-    lang="en"
-    class="light-style layout-menu-fixed"
-    dir="ltr"
-    data-theme="theme-default"
-    data-assets-path="../assets/"
-    data-template="vertical-menu-template-free">
-<?php include '../common/header_link.php'; ?>
 
-<body>
-    <div class="layout-wrapper layout-content-navbar">
-        <div class="layout-container">
-            <!-- Sidebar -->
-            <?php include '../common/sidebar.php'; ?>
-            <!-- / Sidebar -->
 
-            <div class="layout-page">
-                <!-- Navbar -->
-                <?php include '../common/header.php'; ?>
-                <!-- / Navbar -->
+        <!DOCTYPE html>
+        <html
+            lang="en"
+            class="light-style layout-menu-fixed"
+            dir="ltr"
+            data-theme="theme-default"
+            data-assets-path="../assets/"
+            data-template="vertical-menu-template-free">
+        <?php include '../common/header_link.php'; ?>
 
-                <div class="container-p-x">
-                    <div class="card my-4">
-                        <h5 class="card-header">Edit Details</h5>
-                        <div class="card-body">
-                            <form method="POST">
-                                <div class="row mt-3">
-                                    <div class="col-lg-6">
-                                        <div class="form-floating mb-3">
-                                            <input type="text" class="form-control" name="Nameo_f_Equipment" required />
-                                            <label>Name of Equipment</label>
+        <body>
+            <div class="layout-wrapper layout-content-navbar">
+                <div class="layout-container">
+                    <!-- Sidebar -->
+                    <?php include '../common/sidebar.php'; ?>
+                    <!-- / Sidebar -->
+
+                    <div class="layout-page">
+                        <!-- Navbar -->
+                        <?php include '../common/header.php'; ?>
+                        <!-- / Navbar -->
+
+                        <div class="container-p-x">
+                            <div class="card my-4">
+                                <h5 class="card-header">Edit Details</h5>
+                                <div class="card-body">
+
+                                    <form action="edit_programming_lab1.php" method="POST">
+                                        <div class="row mt-3">
+                                            <div class="col-lg-6">
+                                                <input type="hidden" class="form-control" name="update_acc_no" value="<?php echo $row['accession_no']; ?>" />
+
+                                                <div class="form-floating mb-3">
+                                                    <input type="text" class="form-control" name="Name_of_Equipment" value="<?php echo $row['name_of_equipment']; ?>" />
+                                                    <label>Name of Equipment</label>
+                                                </div>
+                                            </div>
+                                            <div class="col-lg-6">
+                                                <div class="form-floating mb-3">
+                                                    <input type="number" step="0.01" class="form-control" name="accession_no" value="<?php echo $row['accession_no']; ?>" />
+                                                    <label>Accession No.</label>
+                                                </div>
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div class="col-lg-6">
-                                        <div class="form-floating mb-3">
-                                            <input type="number" step="0.01" class="form-control" name="Accession_No" required />
-                                            <label>Accession No.</label>
+
+
+                                        <div class="row">
+                                            <div class="col-lg-6">
+                                                <div class="form-floating mb-3">
+                                                    <input type="text" class="form-control" name="Configration" value="<?php echo $row['configration']; ?>" />
+                                                    <label>Configration</label>
+                                                </div>
+                                            </div>
+                                            <div class="col-lg-6">
+                                                <div class="form-floating mb-3">
+                                                    <input type="number" step="0.01" class="form-control" name="Maintenance" value="<?php echo $row['Maintenance']; ?>" />
+                                                    <label>Maintenance</label>
+                                                </div>
+                                            </div>
                                         </div>
-                                    </div>
+
+                                        <div class="row">
+                                            <div class="col-lg-4">
+                                                <div class="form-floating mb-3">
+                                                    <input type="text" class="form-control" name="System_no" value="<?php echo $row['System_no']; ?>" />
+                                                    <label>System no</label>
+                                                </div>
+                                            </div>
+
+
+                                            <div class="col-lg-4">
+                                                <div class="form-floating mb-3">
+                                                    <input type="text" class="form-control" name="GRN_no" value="<?php echo $row['GRN_no']; ?>" />
+                                                    <label>GRN no</label>
+                                                </div>
+                                            </div>
+
+
+
+                                            <div class="col-lg-4">
+                                                <div class="form-floating mb-3">
+                                                    <input type="text" class="form-control" name="Unit_Rate" value="<?php echo $row['Unit_Rate']; ?>" />
+                                                    <label>Unit Rate</label>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div class="row">
+                                            <div class="col-lg-4">
+                                                <div class="form-floating mb-3">
+                                                    <input type="text" class="form-control" name="Quantity" value="<?php echo $row['Quantity']; ?>" />
+                                                    <label>Quantity</label>
+                                                </div>
+                                            </div>
+
+
+                                            <div class="col-lg-4">
+                                                <div class="form-floating mb-3">
+                                                    <input type="text" class="form-control" name="Amount" value="<?php echo $row['Amount']; ?>" />
+                                                    <label>Amount</label>
+                                                </div>
+                                            </div>
+
+
+                                            <div class="col-lg-4">
+                                                <div class="form-floating mb-3">
+                                                    <input type="text" class="form-control" name="Remark" value="<?php echo $row['Remark']; ?>" />
+                                                    <label>Remark</label>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="text-center">
+                                            <button type="submit" name="edit_btn" class="btn btn-primary">Edit</button>
+                                            <a href="./programming_lab1.php" class="btn btn-secondary">Back</a>
+                                        </div>
+                                    </form>
+
+
+
                                 </div>
-
-
-                                <div class="row">
-                                    <div class="col-lg-6">
-                                        <div class="form-floating mb-3">
-                                            <input type="text" class="form-control" name="Configration" required />
-                                            <label>Configration</label>
-                                        </div>
-                                    </div>
-                                    <div class="col-lg-6">
-                                        <div class="form-floating mb-3">
-                                            <input type="number" step="0.01" class="form-control" name="Maintenance" required />
-                                            <label>Maintenance</label>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="row">
-                                    <div class="col-lg-4">
-                                        <div class="form-floating mb-3">
-                                            <input type="text" class="form-control" name="System_no" required />
-                                            <label>System no</label>
-                                        </div>
-                                    </div>
-
-
-                                    <div class="col-lg-4">
-                                        <div class="form-floating mb-3">
-                                            <input type="text" class="form-control" name="GRN_no" required />
-                                            <label>GRN no</label>
-                                        </div>
-                                    </div>
-
-
-
-                                    <div class="col-lg-4">
-                                        <div class="form-floating mb-3">
-                                            <input type="text" class="form-control" name="Unit_Rate" required />
-                                            <label>Unit Rate</label>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="row">
-                                    <div class="col-lg-4">
-                                        <div class="form-floating mb-3">
-                                            <input type="text" class="form-control" name="Quantity" required />
-                                            <label>Quantity</label>
-                                        </div>
-                                    </div>
-
-
-                                    <div class="col-lg-4">
-                                        <div class="form-floating mb-3">
-                                            <input type="text" class="form-control" name="Amount" required />
-                                            <label>Amount</label>
-                                        </div>
-                                    </div>
-
-
-                                    <div class="col-lg-4">
-                                        <div class="form-floating mb-3">
-                                            <input type="text" class="form-control" name="Remark" required />
-                                            <label>Remark</label>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="text-center">
-                                    <button type="submit" class="btn btn-primary">Edit</button>
-                                    <a href="./programming_lab1.php" class="btn btn-secondary">Back</a>
-                                </div>
-                            </form>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-</body>
-
-</html>
+    <?php
+      }
+    } else {
+        echo "no data found by this accession number";
+    }
+    ?>
+        </body>
+     
+            
+        </html>
