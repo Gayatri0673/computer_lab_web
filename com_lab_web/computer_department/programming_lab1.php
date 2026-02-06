27bfd0a5d4a3fe4ca1487f5d72564f6dac0da61b
rohini919 <sonkamblerohini8@gmail.com>
2026-02-06 10:37:27 +0530

    search bar add (#37)

--- com_lab_web/computer_department/programming_lab1.php
+++ com_lab_web/computer_department/programming_lab1.php
@@ -10,10 +10,7 @@ if (isset($_POST['delete_btn'])) {
         header("Location: programming_lab1.php");
     }
 }
-
 ?>
-
-
 <!DOCTYPE html>
 <html
     lang="en"
@@ -25,7 +22,7 @@ if (isset($_POST['delete_btn'])) {
 <?php
 include('../common/header_link.php');
 ?>
-
+<link rel="stylesheet" href="../assets/css/search_bar.css">
 <body>
     <!-- Layout wrapper -->
     <div class="layout-wrapper layout-content-navbar">
@@ -35,7 +32,6 @@ include('../common/header_link.php');
             include '../common/co_hod_sidebar.php';
             ?>
             <!-- / Menu -->
-
             <!-- Layout container -->
             <div class="layout-page">
                 <!-- Navbar -->
@@ -43,31 +39,25 @@ include('../common/header_link.php');
                 include '../common/header.php';
                 ?>
                 <!-- / Navbar -->
-
                 <!-- Content wrapper -->
                 <div class="content-wrapper">
                     <!-- Content -->
-
                     <div class="container-p-x flex-grow-1 container-p-y">
                         <h4 class="text-muted fw-bold py-3 mb-4">COMPUTER CENTER</h4>
-
-
-
-
                         <!-- Hoverable Table rows -->
                         <div class="card">
                             <div class="row">
                                 <div class="col-lg-6 ">
                                     <h5 class="m-4">Programming Lab 1</h5>
                                 </div>
-
                                 <div class="col-lg-6 d-flex justify-content-end">
-                                    <input type="text" class="form-control border-black m-4 shadow-none" placeholder="Search..." aria-label="Search...">
-
+                                    <?php
+                                    include '../common/search_bar.php';
+                                    ?>
                                     <a href="./add_programming_lab1.php">
                                         <button type="button" class="btn btn-primary m-4">ADD+</button>
                                     </a>
-                                     <a href="./programming1_lab_status.php">
+                                    <a href="./programming1_lab_status.php">
                                         <button type="button" class="btn btn-primary m-4">Status</button>
                                     </a>
                                     <div class="text-center">
@@ -75,20 +65,18 @@ include('../common/header_link.php');
                                     </div>
                                 </div>
                             </div>
-<?php
+                            <?php
                             $conn = mysqli_connect("localhost", "root", "", "computer");
                             $query = "SELECT * FROM programming_lab1";
                             $project_run = mysqli_query($conn, $query);
                             $sr = 1;
                             if (mysqli_num_rows($project_run) > 0) {
-
                             ?>
                                 <div class="table-responsive text-nowrap">
-                                    <table class="table table-hover">
+                                    <table id="lab-table" class="table table-hover">
                                         <thead>
                                             <tr>
                                                 <th>Sr.No</th>
-
                                                 <th>Name of Equipment</th>
                                                 <th>Accession No.</th>
                                                 <th>System no</th>
@@ -100,21 +88,13 @@ include('../common/header_link.php');
                                                 <th>Configration</th>
                                                 <th>Maintenance</th>
                                                 <th class="text-center">Action</th>
-
-
                                             </tr>
                                         </thead>
                                         <tbody class="table-border-bottom-0">
                                             <?php
-
                                             while ($project_row = mysqli_fetch_array($project_run)) {
                                             ?>
-
                                                 <tr>
-
-
-
-
                                                     <td><?= $sr++; ?></td>
                                                     <td><?php echo $project_row["name_of_equipment"]; ?></td>
                                                     <td><?php echo $project_row["accession_no"]; ?></td>
@@ -127,9 +107,6 @@ include('../common/header_link.php');
                                                     <td><?php echo $project_row["configration"]; ?></td>
                                                     <td><?php echo $project_row["Maintenance"]; ?></td>
                                                     <td>
-
-
-
                                                         <div>
                                                             <a href="show.php?lab=programming_lab1&accession_no=<?= $project_row['accession_no']; ?>">
                                                                 <button class="btn rounded-pill btn-success me-1">Show</button>
@@ -151,8 +128,6 @@ include('../common/header_link.php');
                                             <?php
                                             }
                                             ?>
-
-                                            <tr>
                                         </tbody>
                                     </table>
                                 <?php
@@ -163,7 +138,10 @@ include('../common/header_link.php');
                                 </div>
                         </div>
 
-            <!-- <tbody class="table-border-bottom-0">
+
+                        
+
+                        <!-- <tbody class="table-border-bottom-0">
                       <tr>
                         <th scope="row">2</th>
                         <td>CPU-LENOVO Desktop Model,LENOVO  Keyboard,LENOVO Mouse,LENOVO Monitor</td>
@@ -2256,18 +2234,18 @@ include('../common/header_link.php');
                 
                 
             </tr> -->
-                      <!-- <?php 
-                      include '../common/dbcon.php';
-                      $sql = "SELECT event_id,title,description,image FROM event"; 
-                      $result = mysqli_query($conn, $sql);
-                      if (mysqli_num_rows($result) > 0) {
-                        while($row = mysqli_fetch_assoc($result)) {
-                          echo "
+                        <!-- <?php
+                                include '../common/dbcon.php';
+                                $sql = "SELECT event_id,title,description,image FROM event";
+                                $result = mysqli_query($conn, $sql);
+                                if (mysqli_num_rows($result) > 0) {
+                                    while ($row = mysqli_fetch_assoc($result)) {
+                                        echo "
                           <tr>
-                            <td><i class=\"fab fa-angular fa-lg text-danger me-3\"></i> <strong>".htmlspecialchars($row['event_id'])."</strong></td>
-                            <td><p class=\"page-para \">".htmlspecialchars($row['title'])."</p></td>
+                            <td><i class=\"fab fa-angular fa-lg text-danger me-3\"></i> <strong>" . htmlspecialchars($row['event_id']) . "</strong></td>
+                            <td><p class=\"page-para \">" . htmlspecialchars($row['title']) . "</p></td>
                             <td>
-                              <img src=\"".htmlspecialchars($row['image'])."\" alt=\"Avatar\" class=\"one\" />
+                              <img src=\"" . htmlspecialchars($row['image']) . "\" alt=\"Avatar\" class=\"one\" />
                             </td>
                             <td>
                          <div> 
@@ -2284,35 +2262,34 @@ include('../common/header_link.php');
                         </td>
                         </tr>
                           ";
-
-                        }
-                      }
-                      ?>
+                                    }
+                                }
+                                ?>
                        -->
-                    </tbody>
-                  </table>
+                        </tbody>
+                        </table>
+                    </div>
                 </div>
-              </div>
-              <!--/ Hoverable Table rows -->
-
-              
-             <hr class="my-5" />
+                <!--/ Hoverable Table rows -->
 
 
-            <!-- Footer -->
-             <!-- Footer -->
-            
-            <!-- / Footer -->
+                <hr class="my-5" />
 
-            <div class="content-backdrop fade"></div>
-          </div>
-          <!-- Content wrapper -->
+
+                <!-- Footer -->
+                <!-- Footer -->
+
+                <!-- / Footer -->
+
+                <div class="content-backdrop fade"></div>
+            </div>
+            <!-- Content wrapper -->
         </div>
         <!-- / Layout page -->
-      </div>
+    </div>
 
-      <!-- Overlay -->
-      <div class="layout-overlay layout-menu-toggle"></div>
+    <!-- Overlay -->
+    <div class="layout-overlay layout-menu-toggle"></div>
     </div>
     <!-- / Layout wrapper -->
 
@@ -2324,12 +2301,13 @@ include('../common/header_link.php');
         >Upgrade to Pro</a
       >
     </div> -->
- <?php
-            include('../common/footer-link.php');
+    <?php
+    include('../common/footer-link.php');
 
 
-            ?>
+    ?>
     <!-- Core JS -->
-   
-  </body>
+
+</body>
+
 </html>
